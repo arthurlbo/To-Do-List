d10bfaff50b0fe467520f68f7fda60cde7318c67
Arthur Carneiro Lobo <arthurlbo16@gmail.com>
2022-04-28 11:04:09 -0300

    estilização

--- js/components/botao.js
+++ js/components/botao.js
@@ -5,9 +5,9 @@ export default function (tarefa) {
     return `
 <div 
     id="${tarefa.id}"
-    class="toast align-items-center ${bgStyle} text-white border-0 show mt-4 d-flex justify-content-between btn" onclick="concluirTarefa ('${tarefa.id}')" -live="assertive" aria-atomic="true>
+    class="toast align-items-center ${bgStyle}  border-0 show mt-4 d-flex justify-content-between btn" onclick="concluirTarefa ('${tarefa.id}')" -live="assertive" aria-atomic="true>
     <div class="d-flex">
-        <div class="toast-body">${tarefa.txt}</div>
+        <div class="toast-body text-black h5">${tarefa.txt}</div>
         <button
             type="button"
             class="btn-close btn-close-white me-2 m-auto"
